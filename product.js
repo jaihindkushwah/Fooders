e8fecb94e27b32ef2f551ef02f0c11dd5158e811
jaiho <jaihind@gmail.code>
2023-03-11 16:25:37 +0530

    fixed the product.html page some code were missed

--- product.js
+++ product.js
@@ -16,50 +16,50 @@ function updateCartIconNo(){
     document.querySelector('#cart>p').innerHTML=Number(addedInCart.length);
     document.querySelector('#cart1>p').innerHTML=Number(addedInCart.length);
 }
+updateCartIconNo();
+// function addInCart(){
+//     const singleProduct=document.querySelectorAll('.singleProduct');
+//     singleProduct.forEach((item)=>{
+//         item.addEventListener('click',(e)=>{
+//         //    console.log(e.target);
+//         console.log(item.firstElementChild.firstElementChild.src);
+//         item.lastElementChild.childNodes.forEach(item=>{console.log(item.className+" "+item.name)})
+//         })
+//     })
+// }
+// addInCart();
 
-function addInCart(){
-    const singleProduct=document.querySelectorAll('.singleProduct');
-    singleProduct.forEach((item)=>{
-        item.addEventListener('click',(e)=>{
-        //    console.log(e.target);
-        console.log(item.firstElementChild.firstElementChild.src);
-        item.lastElementChild.childNodes.forEach(item=>{console.log(item.className+" "+item.name)})
-        })
-    })
-}
-addInCart();
-
-featuredDiv.addEventListener('click',(event)=>{
-    if(event.target.value!=undefined){
-       let name=event.target.name;
-       let price=event.target.value; 
-       let img=event.target.parentElement.style.backgroundImage;
-       addToCart(name,img,price,"XYZ","5","free",'1');
-    }
-});
-console.log(addedInCart);
-function addToCart(name,img,price,shopName,rating,deliveryType,quantity){
-    let curItem={};
-    curItem["name"]=name;
-    curItem["price"]=price; 
-    curItem["img"]=img;
-    curItem["shopName"]=shopName;
-    curItem["rating"]='*'.repeat(Number(rating));
-    curItem["deliveryType"]=deliveryType;
-    curItem['quantity']=quantity;
+// featuredDiv.addEventListener('click',(event)=>{
+//     if(event.target.value!=undefined){
+//        let name=event.target.name;
+//        let price=event.target.value; 
+//        let img=event.target.parentElement.style.backgroundImage;
+//        addToCart(name,img,price,"XYZ","5","free",'1');
+//     }
+// });
+// console.log(addedInCart);
+// function addToCart(name,img,price,shopName,rating,deliveryType,quantity){
+//     let curItem={};
+//     curItem["name"]=name;
+//     curItem["price"]=price; 
+//     curItem["img"]=img;
+//     curItem["shopName"]=shopName;
+//     curItem["rating"]='*'.repeat(Number(rating));
+//     curItem["deliveryType"]=deliveryType;
+//     curItem['quantity']=quantity;
     
-    let curItemIsPresent=itemPresentInTheCart(curItem);
-    if(!curItemIsPresent){
-        addedInCart.push(curItem);
+//     let curItemIsPresent=itemPresentInTheCart(curItem);
+//     if(!curItemIsPresent){
+//         addedInCart.push(curItem);
 
-    }
-    updateCartIconNo();
-}
+//     }
+//     updateCartIconNo();
+// }
 
 
-function itemPresentInTheCart(curItem){
-    return addedInCart.some(el=> JSON.stringify(el)===JSON.stringify(curItem));
-}
+// function itemPresentInTheCart(curItem){
+//     return addedInCart.some(el=> JSON.stringify(el)===JSON.stringify(curItem));
+// }
 
 
 
